54c2c9f2db3890cd8655f695aa58e56076c4b30e
KevinIansyah <keviniansyah04@gmail.com>
2023-06-01 16:21:17 +0700

    cek

--- database/connection.php
+++ database/connection.php
@@ -5,7 +5,7 @@ function connection()
     $dbServer = 'localhost';
     $dbPort = 3306;
     $dbUser = 'root';
-    $dbPass = 'Hubed.com1';
+    $dbPass = '';
     $dbName = 'bayview';
 
     $conn = mysqli_connect($dbServer, $dbUser, $dbPass, $dbName, $dbPort);
