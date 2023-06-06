33ca1931e53b980a5f5cab83cdf23a822693aef2
KevinIansyah <keviniansyah04@gmail.com>
2023-06-07 05:00:43 +0700

    fix lagi

--- detail.php
+++ detail.php
@@ -21,9 +21,6 @@ if (isset($_GET['property'])) {
   <!-- Logo -->
   <link rel="icon" href="image/logoatas.png" type="image/x-icon" />
 
-  <!-- Bootrap -->
-  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous" /> -->
-
   <!-- CSS -->
   <link rel="stylesheet" href="css/app.css" />
 </head>
